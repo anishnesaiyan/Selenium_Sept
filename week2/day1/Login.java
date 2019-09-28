0dace4cba6e4c9d0bf802e6f58be502a36d265dc
TestLeafPages <sarath.muthuswamy@testleaf.com>
2019-09-28 19:06:08 +0530

    week 2 - day 1

---
+++ week2/day1/Login.java
@@ -0,0 +1,32 @@
+package week2.day1;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// Set driver path
+		System.setProperty("webdriver.chrome.driver",
+				"./drivers/chromedriver.exe");
+		// launch browser
+		ChromeDriver driver = new ChromeDriver();
+		// load the url
+		driver.get("http://leaftaps.com/opentaps");
+		// maximize the browser
+		driver.manage().window().maximize();
+		driver.findElementById("username")
+			.sendKeys("DemoSalesManager");
+		driver.findElementById("password")
+			.sendKeys("crmsfa", Keys.ENTER);
+		/*driver.findElementByClassName("decorativeSubmit")
+			.click();*/
+		// close the browser
+		driver.close();
+	}
+}
+
+
+
+
